c0700d556d09f3003e3ef77a0554417e9811faf0
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-04-18 00:52:00 +0100

    Create spring project

---
+++ src/test/java/com/example/minioExample/MinioExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minioExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
